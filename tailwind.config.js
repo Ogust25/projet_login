dc27a9857c246ee4f9290b0eeb9ab1d73c5952ba
Ogu <august.gros@gmail.com>
2022-05-24 11:24:41 +0200

    base de html

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./js/script.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'manrope': ['Manrope', 'sans-serif'],
+        'pressStart': ['Press Start 2P', 'cursive'],
+      },
+      colors: {
+        'bg-main': '#191720',
+        'action-l': '#6871FC',
+        'action-r': '#393DDC',
+        'input': '#1E1C24',
+        'input-b': '#34333C',
+        'input-red': '#D22947',
+        'smTxt': '#616066',
+      },
+    },
+  },
+  plugins: [],
+}
